f5a5df140a092924ef5d839b10ebc1a804a23fcd
Javi <jotaft4@gmail.com>
2022-01-26 22:34:41 +0100

    noske

--- arrays.php
+++ arrays.php
@@ -26,6 +26,22 @@ array("Vuelo"=>"Ryanair-EL00", "Fabricante del avión"=>"Airbus"),
 
 //en la vihen niñio
 
+$arrayduracionviaje=array(
+array("Vuelo"=>"Vueling-AE12", "Duración viaje"=>"120"),
+array("Vuelo"=>"Volotea-EA21", "Duración viaje"=>"100"),
+array("Vuelo"=>"Iberia-WE44", "Duración viaje"=>"340"),
+array("Vuelo"=>"Ryanair-EW44", "Duración viaje"=>"45"),
+array("Vuelo"=>"Vueling-ZK87", "Duración viaje"=>"600"),
+array("Vuelo"=>"Volotea-UA21", "Duración viaje"=>"65"),
+array("Vuelo"=>"Iberia-WH44", "Duración viaje"=>"90"),
+array("Vuelo"=>"Ryanair-EL00", "Duración viaje"=>"50"),
+array("Vuelo"=>"Vueling-AE12", "Duración viaje"=>"115"),
+array("Vuelo"=>"Volotea-EA21", "Duración viaje"=>"100"),
+array("Vuelo"=>"Iberia-WE44", "Duración viaje"=>"85"),
+array("Vuelo"=>"Ryanair-EW44", "Duración viaje"=>"360"),
+);
+
+
 
 
 
