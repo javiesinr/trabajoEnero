a5fba2034d2f2ed570abddca2466504e5ccaa4b3
Javi <jotaft4@gmail.com>
2022-01-26 22:23:39 +0100

    array loko array

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<?php
+
+$destino=array(
+array("Vuelo"=>"Vueling-AE12", "Destino"=>"Milan"),
+array("Vuelo"=>"Volotea-EA21", "Destino"=>"Roma"),
+array("Vuelo"=>"Iberia-WE44", "Destino"=>"Estambul"),
+array("Vuelo"=>"Ryanair-EW44", "Destino"=>"Madrid"),
+array("Vuelo"=>"Vueling-ZK87", "Destino"=>"Lima"),
+array("Vuelo"=>"Volotea-UA21", "Destino"=>"Bilbao"),
+array("Vuelo"=>"Iberia-WH44", "Destino"=>"Roma"),
+array("Vuelo"=>"Ryanair-EL00", "Destino"=>"Madrid"),
+array("Vuelo"=>"Vueling-AE12", "Destino"=>"Roma"),
+array("Vuelo"=>"Volotea-EA21", "Destino"=>"Roma"),
+);
+
+
+
+
+
+
+
+
