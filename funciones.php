776eb0e12b740568bdfacb0483d630c9fc86f42c
Javi <jotaft4@gmail.com>
2022-01-27 21:36:12 +0100

    fun

--- funciones.php
+++ funciones.php
@@ -3,6 +3,13 @@
 include 'arrays.php';
 
 function compañias($compañia,$selct){
-    foreach ($compañias as $compañi)}
+    foreach ($compañias as $compañi){
+        $compañia2=$compañi['Compañias'];
+        $vuelo=$compañi['Vuelos'];
+        if ($vuelo==$select){
+            echo "Su compañia es:".$compañia2."<br>";
+           }
+        }
+    }
 
 ?>
