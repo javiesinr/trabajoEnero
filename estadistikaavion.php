6d7819baa602a429e97a17c29afaef66397f4473
Javi <jotaft4@gmail.com>
2022-01-28 02:23:18 +0100

    en la vihen

---
+++ estadistikaavion.php
@@ -0,0 +1,12 @@
+<?php
+include "funciones.php";
+
+
+$select=$POST["pulsador"]:
+compañia($compañia,$select);
+pajeros ($arraynumeropasajeros,$select);
+destinos ($arraydestino,$select);
+ fabricantes ($arrayfabricante,$select);
+minutosvuelo ($arrayduracionviaje,$select);
+
+?>
